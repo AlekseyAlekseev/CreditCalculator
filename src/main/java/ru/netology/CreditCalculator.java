36d850830387c51263a67dad7a28ed52816eba24
Aleksey Alekseev <a.alekseev5743@yandex.ru>
2021-06-03 16:25:54 +0300

    added program logic

--- src/main/java/ru/netology/CreditCalculator.java
+++ src/main/java/ru/netology/CreditCalculator.java
@@ -2,15 +2,32 @@ package ru.netology;
 
 public class CreditCalculator {
 
+    /**
+     * Расчет ежемесячного платежа по кредиту
+     * @param sum Сумма кредита
+     * @param months Срок погашения в месяцах
+     * @param rates Ставка по кредиту
+     */
     public int getMonthlyPay(int sum, int months, int rates) {
-        return 0;
+        // расчет ежемесячного процента
+        double rate = (double) rates / 100 / 12;
+        // расчет коэффициента аннуитета
+        double coefficient = (rate * Math.pow((1  + rate), months) / ( Math.pow((1 + rate), months ) - 1));
+        // расчет ежемесячного платежа
+        return (int) Math.round(sum * coefficient);
     }
 
+    /**
+     * Расчет суммы переплаты по кредиту
+     */
     public int getOverpayAmount(int monthPay, int months, int sum) {
-        return 0;
+        return (monthPay * months) - sum;
     }
 
+    /**
+     * Расчет общей суммы выплаты
+     */
     public int getTotalPay(int sum, int overPay) {
-        return 0;
+        return sum + overPay;
     }
 }
